c201706ce8ac4ce7d6e8475a3753048cc1b540e1
LvinM <72640082+LvinM@users.noreply.github.com>
2023-08-23 22:32:45 -0600

    Add files via upload

--- web.php
+++ web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\tareaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/', [tareaController::class, 'tarea.index']);
+Route::get('/tarea', [tareaController::class, 'tarea.index']);
+Route::get('/tarea/mostrar/{id}', [tareaController::class, 'tarea.show'])->whereNumber('id');
+Route::get('/tarea/create', [tareaController::class, 'tarea.create']);
+Route::get('/tarea/editar/{id}', [tareaController::class, 'tarea.edit'])->whereNumber('id');
+Route::get('/tarea/eliminar/{id}', [tareaController::class, 'tarea.eliminar'])->whereNumber('id');
+
+
 
 Route::get('/', function () {
     return view('welcome');
